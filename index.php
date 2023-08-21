14a815e92eaffac9ddc59f61ef7e77c7a50f06de
Lordgg007 <supergaudy1@gmail.com>
2023-08-21 10:44:29 -0400

    feat: added footer

--- index.php
+++ index.php
@@ -6,9 +6,24 @@
 
   </main>
 
-  <footer>
-    
-  </footer>
+  
+    <div class="container-f;uid">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <i class="bi bi-bootstrap-fill ms-4"></i><svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+      </div>
+
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-twitter"></i><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-instagram"></i><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary me-4" href="#"><i class="bi bi-facebook"></i><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+      </ul>
+    </footer>
+  </div>
+  
 
 
 
