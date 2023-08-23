d7611502ac43b15ef460b141f246e53f1f36845c
Lordgg007 <supergaudy1@gmail.com>
2023-08-23 10:50:37 -0400

    changed index and added database folder

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+  include("database/db.php");
+?>
+
+<?php
   include("includes/header.php");
 ?>
 
-  <main>
-
+  <main class="container p-4">
+    <div class="row">
+      <div class="col-md-4">
+        <!--MESSAGES-->
+        <?php if(isset($_SESSION['messages'])) ?>
+        <div class="alert alert-?>= $_SESSION['message_type']?= alert-dimissible fade show" role="alert"></div>
+      </div>
+    </div>
   </main>
 
 <?php
